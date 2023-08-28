7118a18bf67d8c90165cc83368648ebd943f2be6
SudeKun <sudekunduroglu@gmail.com>
2023-08-29 02:26:58 +0300

    modified:   main 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <random>
 using namespace std;
 
+
 int main(){
-    cout<<"Hello World";
-    return 0;
+    char arr[8][8];
+    char point = '.';
+    char star = '*';
+
+    for (int i=0;i<8;i++){
+        for (int j=0;j<8;j++){
+            arr[i][j]=point;
+            if (j!=8) cout<<arr[i][j]<<" ";
+            else cout<<arr[i][j];
+        }
+        cout<<endl;
+    }
+
+
 }
